d0929dcb6d1f2c02a258ec4f53e13a6471c8191b
JackForbes <jackforbes24@gmail.com>
2014-09-24 23:27:38 -0400

    Html5Mode

--- js/app.js
+++ js/app.js
@@ -10,13 +10,40 @@ angular.module('myApp', [
   'myApp.directives',
   'myApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('#/', {templateUrl: 'partials/experience.html', controller: 'PageCtrl'});
-  $routeProvider.when('/Experience', {templateUrl: 'partials/experience.html', controller: 'PageCtrl'});
-  $routeProvider.when('/Web-Dev', {templateUrl: 'partials/web-dev.html', controller: 'PageCtrl'});
-  $routeProvider.when('/Resume', {templateUrl: 'partials/resume.html', controller: 'PageCtrl'});
-  $routeProvider.when('/Favourites', {templateUrl: 'partials/favourites.html', controller: 'PageCtrl'});
-  $routeProvider.when('/Interests', {templateUrl: 'partials/interests.html', controller: 'PageCtrl'});
-  $routeProvider.when('/404', {templateUrl: 'partials/404.html', controller: 'PageCtrl'});
-  $routeProvider.otherwise({redirectTo: '/404'});
+config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider.when('#/', {
+    templateUrl: 'partials/experience.html',
+    controller: 'PageCtrl'
+  });
+  $routeProvider.when('/Experience', {
+    templateUrl: 'partials/experience.html',
+    controller: 'PageCtrl'
+  });
+  $routeProvider.when('/Web-Dev', {
+    templateUrl: 'partials/web-dev.html',
+    controller: 'PageCtrl'
+  });
+  $routeProvider.when('/Resume', {
+    templateUrl: 'partials/resume.html',
+    controller: 'PageCtrl'
+  });
+  $routeProvider.when('/Favourites', {
+    templateUrl: 'partials/favourites.html',
+    controller: 'PageCtrl'
+  });
+  $routeProvider.when('/Interests', {
+    templateUrl: 'partials/interests.html',
+    controller: 'PageCtrl'
+  });
+  $routeProvider.when('/404', {
+    templateUrl: 'partials/404.html',
+    controller: 'PageCtrl'
+  });
+  $routeProvider.otherwise({
+    redirectTo: '/',
+    templateUrl: 'partials/experience.html',
+    controller: 'PageCtrl'
+  });
+
+  $locationProvider.html5Mode(true);
 }]);
