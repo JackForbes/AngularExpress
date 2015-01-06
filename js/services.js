36ea6c15c4da2706f0e58bd104a7e71867d268e8
JackForbes <jackforbes24@gmail.com>
2015-01-05 21:43:14 -0800

    Removed Parallax

--- js/services.js
+++ js/services.js
@@ -339,6 +339,10 @@ angular.module('myApp.services', []).
         {
           text: "There is only one way to avoid criticism: Do nothing, say nothing, and be nothing.",
           author: 'Aristotle'
+        },
+        {
+          text: "It is important to view knowledge as sort of a semantic tree -- make sure you understand the fundamental principles, ie the trunk and big branches, before you get into the leaves/details or there is nothing for them to hang on to.",
+          author: 'Elon Musk'
         }
       ],
       gridLetters: 'eyomsrnkvsll404nnmhxvkomvqpageijtnotnfigzdeyoejcmfoundbnjfxlppom'
